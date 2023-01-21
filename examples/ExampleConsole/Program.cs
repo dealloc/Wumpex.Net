8eeff67e8070fd829501188df7618f667f574f5e
Wannes Gennar <wannes.gennar@gmail.com>
2023-01-21 15:21:33 +0100

    Add project files.

---
+++ examples/ExampleConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using Elixus.Discord.Hosted;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder()
+	.ConfigureServices((host, services) =>
+	{
+		services.AddHostedService<HostedDiscordService>();
+	})
+	.Build()
+	.RunAsync();
---
+++ src/Elixus.Discord/Hosted/HostedDiscordService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Elixus.Discord.Hosted;
+
+public sealed class HostedDiscordService : BackgroundService
+{
+	/// <inheritdoc cref="BackgroundService.ExecuteAsync" />
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		//
+	}
+}
